a673c7cbcb5de6e7afc888801eedd567960f6dc0
Kareem-Yahia <144843731+Kareem-Yahia@users.noreply.github.com>
2024-04-09 20:34:07 +0200

    Delete Files directory

--- Files/ram_intf.sv
+++
@@ -1,9 +0,0 @@
-interface ram_if ();
-	parameter ADDR_SIZE=8;
-  logic clk;
-  logic rst_n,rx_valid,tx_valid;
-  logic [9:0] din;
-  logic [ADDR_SIZE-1:0] dout;
-  logic [ADDR_SIZE-1:0] dout_ex;
-  logic tx_valid_ex;
-endinterface 
